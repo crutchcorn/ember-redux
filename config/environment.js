56b4f7bb3784dfe542503558da8037785a71ec7b
Corbin Crutchley <git@crutchcorn.dev>
2024-11-26 10:27:12 -0800

    chore: attempt to auto-deploy

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'ember-redux',
     environment,
-    rootURL: '/',
+    rootURL: '/ember-redux/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
